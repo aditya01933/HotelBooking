d5e31c65ddc19a85c09fb468636a4b9ed94e103c
aditya01933 <adityatiwari01933@gmail.com>
2016-09-24 00:19:17 +0530

    Model Validation for dates added

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,4 +5,15 @@ class Reservation < ActiveRecord::Base
   # This will scope out
   scope :on, -> (checkin_date, checkout_date) {where('checkin_at < ? AND checkout_at > ?', checkout_date, checkin_date) }
 
+  validates :checkin_at, :checkout_at, presence: true
+
+  validate :checkout_at_after_checkin_at
+
+  private
+    def checkout_at_after_checkin_at  
+      return if [checkin_at, checkout_at].any?(&:blank?)            
+      if checkout_at <= checkin_at
+        self.errors.add(:checkout_at, ': Should be greater than checkin date ')
+      end
+    end 
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,5 +60,12 @@ RSpec.configure do |config|
 
 end
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 
 
