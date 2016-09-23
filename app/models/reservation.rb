02e77775491a2d383f9968625cd6d6432854f175
aditya01933 <adityatiwari01933@gmail.com>
2016-09-24 00:49:19 +0530

    6 month booking condition validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,12 +8,28 @@ class Reservation < ActiveRecord::Base
   validates :checkin_at, :checkout_at, presence: true
 
   validate :checkout_at_after_checkin_at
+  validate :checkin_before_6_months
 
   private
+    # Booking should only be for day1 to day2 as booking is done for 
+    # night for example today's 12pm to next day's 12pm.
     def checkout_at_after_checkin_at  
-      return if [checkin_at, checkout_at].any?(&:blank?)            
+      return if dates_blank            
       if checkout_at <= checkin_at
         self.errors.add(:checkout_at, ': Should be greater than checkin date ')
       end
     end 
+
+    # Restriction to book only upto 6 months. User can do checkin within 6 months.
+    # Checkout is not restricted. 
+    def checkin_before_6_months
+      return if dates_blank
+      if checkin_at >= (Date.today + 6.months)
+        self.errors.add(:checkin_at, ': Should be less than or equal to 6 months')
+      end
+    end
+
+    def dates_blank
+      [checkin_at, checkout_at].any?(&:blank?)      
+    end
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -4,4 +4,26 @@ RSpec.describe Reservation, type: :model do
   it { is_expected.to validate_presence_of(:checkin_at) }
   it { is_expected.to validate_presence_of(:checkout_at) }
 
+  context "when checkin after checkout" do
+    it "should validate checkout after checkin" do
+      reservation = FactoryGirl.build(:reservation, checkout_at: Date.today, checkin_at: Date.today + 1)
+      expect(reservation).to be_invalid
+      expect(reservation.errors[:checkout_at][0]).to include(': Should be greater than checkin date')
+    end
+  end
+
+  context "when checkin equal to checkout" do
+    it "should validate checkout after checkin" do
+      reservation = FactoryGirl.build(:reservation, checkout_at: Date.today, checkin_at: Date.today)
+      expect(reservation).to be_invalid
+      expect(reservation.errors[:checkout_at][0]).to include(': Should be greater than checkin date')
+    end
+  end
+
+  it "should validate booking before 6 months" do
+   reservation = FactoryGirl.build(:reservation, checkout_at: Date.today + 6.months + 2.days, checkin_at: Date.today + 6.months + 1.day)
+   expect(reservation).to be_invalid
+   expect(reservation.errors[:checkin_at][0]).to include(': Should be less than or equal to 6 months') 
+  end
+
 end
